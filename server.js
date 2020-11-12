ebb5f010abc6d8e90cd20c01099938f793db979b
Janice-M <janiceink001@gmail.com>
2020-11-12 23:55:19 +0300

    run the mothafocken server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ fastify.get('/', async (request, reply) => {
 const start = async () => {
     try {
         await fastify.listen(3000)
-      fastify.log.info(`server listening on ${fastify.server.address().port}`)
+        fastify.log.info(`server listening on ${fastify.server.address().port}`)
     } catch (err) {
       fastify.log.error(err)
       process.exit(1)
